84e73664f10d7b3d65510b8a0120d7d03507dc9d
Josh Getter <getterj@mail.gvsu.edu>
2017-01-30 12:48:21 -0500

    Added storyboard items and began calculation for distance.

--- GeoCalculator/ViewController.swift
+++ GeoCalculator/ViewController.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var p1Long: UITextField!
+    @IBOutlet weak var p2Long: UITextField!
+    @IBOutlet weak var p1Lat: UITextField!
+    @IBOutlet weak var p2Lat: UITextField!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var bearingLabel: UILabel!
+    @IBAction func calcClick(_ sender: Any) {
+        let lat1 = CLLocationDegrees(Double(p1Lat.text!)!)
+        let long1 = CLLocationDegrees(Double(p1Long.text!)!)
+        let lat2 = CLLocationDegrees(Double(p2Lat.text!)!)
+        let long2 = CLLocationDegrees(Double(p2Long.text!)!)
+        var loc1 : CLLocation = CLLocation(latitude: lat1, longitude: long1)
+        var loc2 = CLLocation(latitude: lat2, longitude: long2)
+        let distance = loc1.distance(from: loc2)
+        distanceLabel.text = "Distance: " + String(distance)
+        
+    }
+    @IBAction func clearClick(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
