c7befa9129190aa7b1e5c11fa5b13417850bf0b1
Josh Getter <getterj@mail.gvsu.edu>
2017-01-30 12:05:28 -0500

    Initial Commit

---
+++ GeoCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoCalculator
+//
+//  Created by Josh Getter on 1/30/17.
+//  Copyright © 2017 Josh Getter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
