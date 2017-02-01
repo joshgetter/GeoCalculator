01c92623355b2138c4d1b6f21fa09b4b2f4415d7
Josh Getter <getterj@mail.gvsu.edu>
2017-02-01 08:35:28 -0500

    Added bearing calculations/rounding. Also added decimal with minus keyboard, and clicks out of focus now dismiss the keyboard.

--- GeoCalculator/ViewController.swift
+++ GeoCalculator/ViewController.swift
@@ -17,15 +17,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var bearingLabel: UILabel!
     @IBAction func calcClick(_ sender: Any) {
+        guard (p1Long.text?.isEmpty)! == false && (p1Lat.text?.isEmpty) == false && (p2Lat.text?.isEmpty)! == false && (p2Long.text?.isEmpty)! == false else {
+            return
+        }
         let lat1 = CLLocationDegrees(Double(p1Lat.text!)!)
         let long1 = CLLocationDegrees(Double(p1Long.text!)!)
         let lat2 = CLLocationDegrees(Double(p2Lat.text!)!)
         let long2 = CLLocationDegrees(Double(p2Long.text!)!)
-        var loc1 : CLLocation = CLLocation(latitude: lat1, longitude: long1)
-        var loc2 = CLLocation(latitude: lat2, longitude: long2)
+        let loc1 : CLLocation = CLLocation(latitude: lat1, longitude: long1)
+        let loc2 = CLLocation(latitude: lat2, longitude: long2)
         let distance: Double = Double((loc1.distance(from: loc2)) / 1000)
         let distanceRounded = round(100 * distance)/100
         distanceLabel.text = "Distance: " + String(distanceRounded) + " kilometers"
+        let bearing = loc1.bearingToPoint(point: loc2)
+        let bearingRounded = round(bearing * 100)/100
+        bearingLabel.text = "Bearing: " + String(bearingRounded) + " degrees"
         
     }
     @IBAction func clearClick(_ sender: Any) {
@@ -44,6 +50,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
