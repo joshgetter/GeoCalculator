89b311b8d9b4f1e400fa0ad093b8f672a31ef178
Josh Getter <getterj@mail.gvsu.edu>
2017-02-15 12:42:58 -0500

    Added new view controllers and navigation controller, cancel button currently works

---
+++ GeoCalculator/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  GeoCalculator
+//
+//  Created by Josh Getter on 2/15/17.
+//  Copyright © 2017 Josh Getter. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SettingsViewController : UIViewController{
+    @IBAction func cancelClick(_ sender: Any) {
+        self.dismiss(animated: true) {
+        }
+    }
+    @IBAction func saveClick(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        
+    }
+}
